20e067e66a822b77cc365768a85ef2a792c8274b
cona-bear <seonghyeon.koo@bearrobotics.ai>
2025-03-10 17:49:54 +0900

    add code

---
+++ src/main.py
@@ -0,0 +1,84 @@
+import os
+import pytz 
+import datetime
+
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+
+SLACK_TOKEN = os.getenv("SLACK_TOKEN")
+CHANNEL_ID = os.getenv("CHANNEL_ID")
+
+
+def build_message():
+
+    kst = pytz.timezone("Asia/Seoul")
+    today = datetime.datetime.now(kst).strftime("%m/%d")
+
+    return [{
+			"type": "header",
+			"text": {
+				"type": "plain_text",
+				"text": f":maru_is_a_puppy: {today} Today's Brain Exercise :maru_is_a_puppy:"
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "Hey Genius, *BRING IT ON!!!!!*"
+			}
+		},
+		{
+			"type": "actions",
+			"elements": [
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"text": ":capital_abcd: WORDLE"
+					},
+					"url": "https://www.nytimes.com/games/wordle"
+				},
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"text": ":jigsaw: 꼬들"
+					},
+					"url": "https://kordle.kr"
+				},
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"text": ":quad_parrot: Quordle"
+					},
+					"url": "https://www.merriam-webster.com/games/quordle"
+				}
+			]
+		},
+		{
+			"type": "divider"
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "plain_text",
+				"text": "maru luvs you. maru is a puppy. :maru_heart:",
+			}
+		},
+    ]
+
+
+if __name__ == "__main__":
+    client = WebClient(token=SLACK_TOKEN)
+    
+    try:
+        response = client.chat_postMessage(
+            channel=CHANNEL_ID,
+            blocks=build_message(),
+            unfurl_links=False, # Don't show preview of the link
+        )
+    except SlackApiError as e:
+        assert e.response["error"]
