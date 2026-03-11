d48361e00389b9bb9044fe03b16b72ef684713d6
cona-bear <seonghyeon.koo@bearrobotics.ai>
2026-03-11 11:14:04 +0900

    change header text: stans

--- src/main.py
+++ src/main.py
@@ -96,7 +96,7 @@ def build_message():
     today = datetime.datetime.now(kst).strftime("%m/%d")
     seed = datetime.datetime.now(kst).strftime("%Y%m%d")
 
-    header_text = f"{today} High key, You are Genius"
+    header_text = f"{today} Hi stans, Delulu is a Solulu"
     if today == "02/21":
         header_text = f"{today} Happy Birthday, All Hail Queen Cona!"
 
