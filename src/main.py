54f6066cb508251ce89570cd1e49a18702d9a236
cona-bear <seonghyeon.koo@bearrobotics.ai>
2026-03-24 11:10:15 +0900

    change header text: massive L fr

--- src/main.py
+++ src/main.py
@@ -101,7 +101,7 @@ def build_message():
     today = datetime.datetime.now(kst).strftime("%m/%d")
     seed = datetime.datetime.now(kst).strftime("%Y%m%d")
 
-    header_text = f"{today} Can you beat the Cona Bot?"
+    header_text = f"{today} Missing today would be a massive L fr"
     if today == "02/21":
         header_text = f"{today} Happy Birthday, All Hail Queen Cona!"
 
