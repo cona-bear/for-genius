92808ff80eb259624e0eef4b1b637e8e037b013a
Cona <seonghyeon.koo@bearrobotics.ai>
2026-05-13 14:32:33 +0900

    change header: supercalifragelisticexpialidocious

--- src/main.py
+++ src/main.py
@@ -101,7 +101,7 @@ def build_message():
     today = datetime.datetime.now(kst).strftime("%m/%d")
     seed = datetime.datetime.now(kst).strftime("%Y%m%d")
 
-    header_text = f"{today} Don't play hooky. I feel lonely. Can you be my friend?"
+    header_text = f"{today} I miss you, Supercalifragilisticexpialidocious"
     if today == "02/21":
         header_text = f"{today} Happy Birthday, All Hail Queen Cona!"
 
